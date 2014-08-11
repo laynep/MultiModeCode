43cb5805de7ae6c4814fd45d948193a5af4fca05
layne <lpri691@aucklanduni.ac.nz>
2014-08-11 19:34:18 +1200

    modified:   model_params/cons_rel/cons_rel_submit.py

--- model_params/cons_rel/cons_rel_submit.py
+++ model_params/cons_rel/cons_rel_submit.py
@@ -13,7 +13,7 @@ approx_list = ["deltaN", "HCA"]
 range_unif = ["1.0e-14", "1.0e-13"]
 range_log = ["-14", "-12"]
 
-njobs = 10
+njobs = 2
 
 def run_cmd(cmd):
     proc = subprocess.Popen(cmd,
@@ -26,7 +26,7 @@ def modify_pfile(nf, prior, approx, p):
 
     #Number of fields
     sed_cmd = "sed -i \"s/num_inflaton =.*/num_inflaton = %s/g\" " %nf +pfile
-    print sed_cmd
+    #print sed_cmd
     run_cmd(sed_cmd)
 
     #Approximation scheme
@@ -36,7 +36,7 @@ def modify_pfile(nf, prior, approx, p):
     else:
         sed_cmd = "sed -i \"s/use_horizon_cross_approx =.*/" \
             "use_horizon_cross_approx = %s/g\" " %(".false.") +pfile
-    print sed_cmd
+    #print sed_cmd
     run_cmd(sed_cmd)
 
     if prior is "unif":
@@ -45,33 +45,33 @@ def modify_pfile(nf, prior, approx, p):
         sed_cmd = "sed -i \"s/param_sampling =.*/"\
                 "param_sampling = %s/g\" " %(2) +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
         #p exponent
         sed_cmd = "sed -i \"s/vp_prior_min(2,:) =.*/"\
                 "vp_prior_min(2,:) = %s/g\" " %p +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
         sed_cmd = "sed -i \"s/vp_prior_max(2,:) =.*/"\
                 "vp_prior_max(2,:) = %s/g\" " %p +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
         #Param priors
         sed_cmd = "sed -i \"s/vp_prior_min(1,:) =.*/"\
                 "vp_prior_min(1,:) = %s/g\" " %range_unif[0] +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
         sed_cmd = "sed -i \"s/vp_prior_max(1,:) =.*/"\
                 "vp_prior_max(1,:) = %s/g\" " %range_unif[1] +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
 
@@ -81,55 +81,64 @@ def modify_pfile(nf, prior, approx, p):
         sed_cmd = "sed -i \"s/param_sampling =.*/"\
                 "param_sampling = %s/g\" " %(3) +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
         #p exponent
         sed_cmd = "sed -i \"s/vp_prior_min(2,:) =.*/"\
                 "vp_prior_min(2,:) = %s/g\" " %(np.log10(p)) +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
         sed_cmd = "sed -i \"s/vp_prior_max(2,:) =.*/"\
                 "vp_prior_max(2,:) = %s/g\" " %(np.log10(p)) +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
         #Param priors
         sed_cmd = "sed -i \"s/vp_prior_min(1,:) =.*/"\
                 "vp_prior_min(1,:) = %s/g\" " %range_log[0] +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
         sed_cmd = "sed -i \"s/vp_prior_max(1,:) =.*/"\
                 "vp_prior_max(1,:) = %s/g\" " %range_log[1] +pfile
 
-        print sed_cmd
+        #print sed_cmd
         run_cmd(sed_cmd)
 
 for nf in first_nf_list:
     for prior in prior_list:
         for approx in approx_list:
 
+	    newdir = "newrun_p%s_Nf%s_%s_%s" %(2.0,nf,prior,approx)
+	    sedcmd = "sed -i \"s/multifield.*/multifield\/NEWRUN_CONSREL\/%s/g\" "%newdir + "slurm_job.sh"
+            run_cmd(sedcmd)
+
             modify_pfile(nf, prior, approx, p=2.0)
 
             #Submit the jobs
             submitcmd = ". ./submit.sh " + str(njobs)
 
-            print submitcmd
+            #print submitcmd
             run_cmd(submitcmd)
 
 for nf in second_nf_list:
     for prior in prior_list:
         for approx in approx_list:
             for p in p_list:
+
+	    	newdir = "newrun_p%s_Nf%s_%s_%s" %(p,nf,prior,approx)
+	    	sedcmd = "sed -i \"s/multifield.*/multifield\/NEWRUN_CONSREL\/%s/g\" "%newdir + "slurm_job.sh"
+            	run_cmd(sedcmd)
+
                 modify_pfile(nf, prior, approx, p)
 
                 #Submit the jobs
                 submitcmd = ". ./submit.sh " + str(njobs)
 
-                print submitcmd
+                #print submitcmd
                 run_cmd(submitcmd)
